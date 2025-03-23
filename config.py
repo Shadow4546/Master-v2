86116c433409b4129d303baf1e619e8d350c1bac
Shadow4546 <89007456+Shadow4546@users.noreply.github.com>
2025-03-23 08:56:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
-    VIP_USER = os.environ.get('VIP_USERS', '').split(',')
-    VIP_USERS = [int(5357048091) for user_id in VIP_USER]
+    VIP_USER = os.environ.get('VIP_USERS', '7170328188').split(',')
+    VIP_USERS = [int(7170328188) for user_id in VIP_USER]
